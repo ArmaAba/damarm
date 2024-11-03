514c72a251a112292a2d906b692a9bf42bec816b
adambrauskas <aba@example.com>
2024-11-03 19:32:59 +0200

    api integration and deploy with lambda

---
+++ task03/src/lambdas/hello_world/handler.py
@@ -0,0 +1,48 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+import json
+
+_LOG = get_logger('HelloWorld-handler')
+
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+
+    def handle_request(self, event, context):
+        path = event.get("path", "/")
+        method = event["requestContext"]["httpMethod"]
+        if  method == "GET" and path == "/hello":
+            response_body = {
+                "statusCode": 200,
+                "message": "Hello from Lambda",
+            }
+            response = {
+                "statusCode": 200,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": json.dumps(response_body)
+            }
+            return response
+
+        else:
+            error_message = {
+                "statusCode": 400,
+                "message": f"Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}"
+            }
+            error_response = {
+                "statusCode": 400,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": json.dumps(error_message)
+            }
+            return error_response
+
+HANDLER = HelloWorld()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
