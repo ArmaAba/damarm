f39f144532ab3b52fa5c673bc42423db12119737
adambrauskas <aba@example.com>
2024-11-11 18:55:21 +0200

    task07

---
+++ task07/src/lambdas/uuid_generator/handler.py
@@ -0,0 +1,51 @@
+import uuid
+import boto3
+import datetime
+import json
+import os
+
+# Assuming `get_logger` and `AbstractLambda` are correctly defined
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('UuidGenerator-handler')
+
+# Initialize the S3 client
+s3_client = boto3.client('s3')
+
+
+class UuidGenerator(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+
+    def handle_request(self, event, context):
+        try:
+            uuids = [str(uuid.uuid4()) for _ in range(10)]
+            _LOG.info(f"Generated UUIDs: {uuids}")
+            data = json.dumps({"uuids": uuids})
+            timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+            file_name = f"uuids_{timestamp}.json"
+            _LOG.info(f"File name for UUID storage: {file_name}")
+            bucket_name = os.environ.get("target_table")
+            _LOG.info(f"Target S3 bucket: {bucket_name}")
+            s3_client.put_object(
+                Bucket=bucket_name,
+                Key=file_name,
+                Body=data,
+                ContentType='application/json'
+            )
+            _LOG.info(f"Successfully stored UUIDs in S3 bucket '{bucket_name}' with filename '{file_name}'")
+
+            return {"statusCode": 200, "body": f"Stored UUIDs in '{file_name}'"}
+
+        except Exception as e:
+            _LOG.error(f"Error occurred while handling the request: {str(e)}", exc_info=True)
+            return {"statusCode": 500, "body": f"Error occurred: {str(e)}"}
+
+
+HANDLER = UuidGenerator()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
